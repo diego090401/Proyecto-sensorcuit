e2c757c6297b1edcdf24321f4785f829009695c6
Diego Alonso Espinoza Rojas <diegoalonso0904@gmail.com>
2021-11-19 17:48:30 -0600

    Cambio en la aplicacion

--- Aplicacion_web/app.py
+++ Aplicacion_web/app.py
@@ -1,4 +1,10 @@
 import Base_datos as db
 from flask import Flask, redirect, render_template, url_for
 import flask
-
+app = Flask(__name__)
+@app.route("/")
+def root ():
+   return redirect("home")
+@app.route("/home")
+def Home ():
+    return "Hola mundo"
