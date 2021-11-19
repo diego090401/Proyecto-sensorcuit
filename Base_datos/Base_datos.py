db23a56ce9a7f51beeb2338baf98910c621d8de7
Diego Alonso Espinoza Rojas <diegoalonso0904@gmail.com>
2021-11-19 17:26:50 -0600

    Creacion de conexion a base de datos

---
+++ Base_datos/Base_datos.py
@@ -0,0 +1,39 @@
+import mysql.connector
+from mysql.connector import cursor
+
+class DATABASE():
+    DATAB = mysql.connector.connect(
+    host = "sensorcuit.cyuavfc10r3q.us-east-2.rds.amazonaws.com",
+    user = "Diego_0904",
+    password = 'HepsuMI0QeS223Cphghh',
+    database = 'Proyecto',
+    )
+    cursor = DATAB.cursor()
+    def InsertIntoUsuarios(DATAB= DATAB, cursor=cursor):
+        NewUsername = str(input("Ingrese su nombre de usuario :"))
+        NewMail = str(input("Ingrese su email: "))
+        NewPassword = str(input("Ingrese su constraseña: "))
+        NewUser = (NewUsername, NewMail, NewPassword)
+        InsertIntoUsuarios = "insert into usuarios(username, user_mail, user_password) values (%s, %s, %s)"
+        cursor.execute(InsertIntoUsuarios, NewUser)
+        DATAB.commit()
+        
+    def UpdateUsername(DATAB= DATAB, cursor=cursor) : 
+        
+        LastaUsername, UpdatedUsername=  str(input("Ingrese su anterior nombre de usuario: ")), str(input("Ingrese su nuevo nombre de usuario: "))
+        UpdatedData = (UpdatedUsername, LastaUsername)
+        UpdateUsername = "update usuarios set username = %s where username = %s"
+        cursor.execute(UpdateUsername, UpdatedData)
+        DATAB.commit()
+
+    def ShowUsuarioTable(DATAB= DATAB, cursor=cursor):
+        cursor.execute("select * from Usuario")
+        RawUsuarioTable =cursor.fetchall()
+        
+        return RawUsuarioTable
+    def DeleteUser (DATAB= DATAB, cursor=cursor):
+        DeleteUser = "delete from usuarios where usuario_id = %s"
+        Usuario_id = (4, )
+        cursor.execute(DeleteUser, Usuario_id)
+        DATAB.commit()
+print(DATABASE.ShowUsuarioTable())
